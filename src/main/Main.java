4fd9e4acc020bf7668808a779a27292cf7e87599
Erica <erica.abdao@zup.com.br>
2025-01-03 14:42:59 -0300

    refactor: Acrescenta a classe e o package menu. E tira o menu da classe main.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
+import menu.Menu;
 import model.FileHandler;
 import model.VideoManager;
 import repository.FileVideoRepository;
 
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -12,18 +14,11 @@ public class Main {
         FileHandler videoService = new FileHandler(new FileVideoRepository("videos.txt"));
         VideoManager videoManager = new VideoManager(new FileVideoRepository("videos.txt"));
 
-        while (true) {
-            System.out.println("\n=== Sistema de Gerenciamento de Vídeos ===");
-            System.out.println("1. Adicionar vídeo");
-            System.out.println("2. Listar vídeos");
-            System.out.println("3. Pesquisar vídeo por título");
-            System.out.println("4. Editar vídeo.");
-            System.out.println("5. Excluir vídeo.");
-            System.out.println("6. Filtrar vídeos por categoria.");
-            System.out.println("7. Ordenar vídeos por data de publicação.");
-            System.out.println("8. Exibir relatório de estatísticas.");
-            System.out.println("9. Sair.");
-            System.out.print("Escolha uma opção: ");
+        boolean isMenuOpen = true;
+
+        while (isMenuOpen) {
+            Menu.show();
+
             int opcao = scanner.nextInt();
             scanner.nextLine();
 
@@ -45,7 +40,7 @@ public class Main {
                 System.out.println("Produzindo");
             } else if (opcao == 9) {
                 System.out.println("Saindo do sistema...");
-                break;
+                isMenuOpen = false;
             } else {
                 System.out.println("Opção inválida. Digite uma opção do menu!");
             }
